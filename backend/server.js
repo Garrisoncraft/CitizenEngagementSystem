12cb254b41808f9157c83413dd00fd06f8baf9e3
Garrisoncraft <garrisonsayor@gmail.com>
2025-05-17 08:49:27 +0200

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const app = express();
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 // Routes
 const submissionsRouter = require('./routes/submissions');
 const adminRouter = require('./routes/admin');
 const authRouter = require('./routes/auth');
 
 
+const app = express();
 const port = process.env.PORT || 5000;
+
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || '*',
+  optionsSuccessStatus: 200,
+};
 
 
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 
